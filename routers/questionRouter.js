0e2373bcf40ec1c302ec888153eea0a6fbad2f52
Nandhini-K-01 <85298236+Nandhini-K-01@users.noreply.github.com>
2022-12-23 01:39:09 +0530

    Update questionRouter.js

--- routers/questionRouter.js
+++ routers/questionRouter.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get("/get", questionModule.getQuestions);
 router.get("/get/:id", questionModule.getQuestionById);
+router.put("/put/:id", questionModule.updateById);
 router.post("/create", questionModule.createQuestion);
 
 module.exports = router;
